3af4aff17fd1cdaae3b00e9147f4047b6a5af434
grokfu <neil.t.anderson@gmail.com>
2011-07-19 22:26:23 +0100

    Market unit tests

--- OrderMatchingEngineTests/UnitTests/MarketTests.cs
+++ OrderMatchingEngineTests/UnitTests/MarketTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using NUnit.Framework;
 using OrderMatchingEngine;
 using OrderMatchingEngine.Exchange;
@@ -56,7 +57,31 @@ namespace OrderMatchingEngineTests.UnitTests
         [Test]
         public void SubmitMultipleOrdersConcurrently()
         {
-            
+            var orders = new List<Order>(Orders());
+
+
+            foreach (var order in orders)
+            {
+                Order order1 = order;
+                ThreadPool.QueueUserWorkItem((e) => m_Market.SubmitOrder(order1));
+            }
+
+            while (m_OrderBook.SellOrders.Count() < 2 && m_OrderBook.BuyOrders.Count() < 2) ;
+
+            Assert.That(orders[0], Is.EqualTo(m_OrderBook.BuyOrders[1]));
+            Assert.That(orders[1], Is.EqualTo(m_OrderBook.BuyOrders[0]));
+            Assert.That(orders[2], Is.EqualTo(m_OrderBook.SellOrders[0]));
+            Assert.That(orders[3], Is.EqualTo(m_OrderBook.SellOrders[1]));
+        }
+
+        private IEnumerable<Order> Orders()
+        {
+            for(int i=0; i < 2; ++i)
+                yield return new EquityOrder(m_Instrument, Order.OrderTypes.GoodUntilCancelled, Order.BuyOrSell.Buy, i, (ulong)i);
+
+            for (int i = 0; i < 2; ++i)
+                yield return new EquityOrder(m_Instrument, Order.OrderTypes.GoodUntilCancelled, Order.BuyOrSell.Sell, i, (ulong)i);
+
         }
     }
 }
