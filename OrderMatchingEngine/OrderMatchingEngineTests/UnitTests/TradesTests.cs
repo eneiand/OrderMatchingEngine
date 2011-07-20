591d92a01e6e48c80c3e8cc6e80bcc257afb1fc1
grokfu <neil.t.anderson@gmail.com>
2011-07-20 14:17:00 +0100

    TradesTests

---
+++ OrderMatchingEngine/OrderMatchingEngineTests/UnitTests/TradesTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using OrderMatchingEngine;
+using OrderMatchingEngine.OrderBook;
+
+namespace OrderMatchingEngineTests.UnitTests
+{
+    [TestFixture]
+    class TradesTests
+    {
+        private Trades m_Trades;
+        private Instrument m_Instrument;
+
+        [SetUp]
+        public void Init()
+        {
+            m_Instrument = new Instrument("MSFT");
+            m_Trades = new Trades(m_Instrument);
+        }
+
+        [Test]
+        public void AddTradeTest()
+        {
+            var trade = new Trade(m_Instrument, 100UL, 100.10M);
+            m_Trades.AddTrade(trade);
+            
+            Assert.That(((Trades.InMemoryTradeProcessor)m_Trades.TradeProcessingStrategy).Trades[0], Is.EqualTo(trade));
+        }
+    }
+}
