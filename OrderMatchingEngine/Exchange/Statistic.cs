db40006fb0fac857a7d5ddd896a7df47c357fdc0
grokfu <neil.t.anderson@gmail.com>
2011-07-21 21:13:50 +0100

    beginning of Statistics

---
+++ OrderMatchingEngine/Exchange/Statistic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace OrderMatchingEngine.Exchange
+{
+    public class Statistic
+    {
+        private long m_Value = 0;
+        private readonly Object m_Locker = new Object();
+
+        public Statistic(String name)
+        {
+            if (name == null) throw new ArgumentNullException("name");
+            Name = name;
+        }
+
+        public String Name { get; private set; }
+        public long Value { get { lock (m_Locker) return m_Value; }}
+
+        public static Statistic operator++(Statistic stat)
+        {
+            Interlocked.Increment(ref stat.m_Value);
+            return stat;
+        }
+    }
+}
